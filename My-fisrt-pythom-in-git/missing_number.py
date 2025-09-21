95355ade989b25f39f21d15eb4edd6d97c5dc30b
GonJi-sama <tammarit.g@gmail.com>
2025-09-21 18:03:50 +0700

    v.1

--- My-fisrt-pythom-in-git/missing_number.py
+++ My-fisrt-pythom-in-git/missing_number.py
@@ -1,5 +1,5 @@
-nums = [9, 6, 4, 2, 3, 5, 7, 0, 1]
+nums = [8, 6, 4, 2, 3, 5, 7, 0, 1] # Example input
 
-n = len(nums)
-res = n * (n + 1) // 2
+n = len(nums) # Since one number is missing, the length of nums is n
+res = n * (n + 1) // 2 # Sum of numbers from 0 to n
 print(res - sum(nums))
