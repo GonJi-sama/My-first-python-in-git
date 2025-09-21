359363893ff74e9a5cdbbaf9dd9f6be957beeacb
GonJi-sama <tammarit.g@gmail.com>
2025-09-21 18:07:26 +0700

    v.1

--- My-fisrt-pythom-in-git/missing_number.py
+++ My-fisrt-pythom-in-git/missing_number.py
@@ -2,4 +2,4 @@ nums = [8, 6, 4, 2, 3, 5, 7, 0, 1] # Example input
 
 n = len(nums) # Since one number is missing, the length of nums is n
 res = n * (n + 1) // 2 # Sum of numbers from 0 to n
-print(res - sum(nums))
+print(res - sum(nums)) # The missing number is the difference between the expected sum and the actual sum
